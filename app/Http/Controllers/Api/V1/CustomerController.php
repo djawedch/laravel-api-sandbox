a131674d5624d30cc70dbfbca5561fb03b95044c
djawedch <120687949+djawedch@users.noreply.github.com>
2026-03-29 02:33:34 +0100

    Implement create customer endpoint with validation via StoreCustomerRequest

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
@@ -30,7 +30,7 @@ class CustomerController extends Controller
 
     public function store(StoreCustomerRequest $request)
     {
-        
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     public function show(Customer $customer)
