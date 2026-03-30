a012b8d2af828b509b1da17ac61a8fea51203628
djawedch <120687949+djawedch@users.noreply.github.com>
2026-03-30 04:59:53 +0100

    feat: protect API routes with Sanctum authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\{Auth, Route, Hash};
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function() {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('admin-token', ['create', 'update']);
+            $basicToken = $user->createToken('admin-token');
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $adminToken->plainTextToken,
+                'basic' => $adminToken->plainTextToken,
+            ];
+        }
+    }
 });
