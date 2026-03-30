01350ef27b1d274eadaa2ef42c34f4ebc5dc5741
djawedch <120687949+djawedch@users.noreply.github.com>
2026-03-30 01:46:02 +0100

    Implement update customer endpoint with PUT and PATCH support via UpdateCustomerRequest

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\{StoreCustomerRequest, UpdateCustomerRequest};
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\{CustomerResource, CustomerCollection};
@@ -46,7 +45,7 @@ class CustomerController extends Controller
 
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        
+        $customer->update($request->all());
     }
 
     public function destroy(Customer $customer)
