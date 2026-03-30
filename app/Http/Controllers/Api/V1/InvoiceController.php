94102d7dd54a4ab193a807299eb0361b75ffea0f
djawedch <120687949+djawedch@users.noreply.github.com>
2026-03-30 02:38:23 +0100

    feat: implement bulk insert endpoint for invoices

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\V1\{StoreInvoiceRequest, UpdateInvoiceRequest};
+use App\Http\Requests\V1\{StoreInvoiceRequest, UpdateInvoiceRequest, BulkStoreInvoiceRequest};
 use App\Models\Invoice;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\{InvoiceResource, InvoiceCollection};
 use Illuminate\Http\Request;
 use App\Filters\V1\InvoicesFilter;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -28,6 +29,15 @@ class InvoiceController extends Controller
     public function store(StoreInvoiceRequest $request)
     {
         //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request) 
+    {
+        $bulk = collect($request->all())->map(function($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
     }
 
     public function show(Invoice $invoice)
